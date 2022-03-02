ce6cd890e85bc412ca20b684f7d1f4e2514d92b3
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-01 21:20:24 -0300

    :sparkles: feat: hash password for create user

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+
 
 from .models import User
 
@@ -7,4 +9,9 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username', 'email', 'password', 'first_name',
+                  'last_name', 'gender', 'phone', 'birthday')
+    
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
