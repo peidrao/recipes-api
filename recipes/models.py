2a595222a913fb0f6d9d24c643b8f758a3cabec0
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-03 20:04:43 -0300

    :heavy_plus_sign: feat: Pillow

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,16 @@
+import os
+import uuid
+
 from django.db import models
 from django.db.models import signals
 from django.template.defaultfilters import slugify
+
+
+def recipe_image_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('images/recipe/', filename)
 
 
 class Tag(models.Model):
@@ -14,8 +24,28 @@ class Tag(models.Model):
         return str(self.name)
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    ingredients = models.ManyToManyField(Ingredient, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+
 def slug_pre_save(signal, instance, sender, **kwargs):
     instance.slug = slugify(instance.name)
-
 
 signals.pre_save.connect(slug_pre_save, sender=Tag)
--- recipes/views.py
+++ recipes/views.py
@@ -13,9 +13,3 @@ class TagViewSet(viewsets.ModelViewSet):
 class CategoryListView(generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = CategorySerializer
-
-    # def list(self, request, *args, **kwargs):
-    #     queryset = self.filter_queryset(self.get_queryset())
-
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     return Response(serializer.data)
