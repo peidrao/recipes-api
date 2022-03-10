3bf739a2087e1cf086188b68d3551dc1d833450c
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-09 21:58:56 -0300

    :recycle: refactor: move urls

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,22 +2,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 
-
-from authentication.views import UserViewSet
-
-router = routers.DefaultRouter()
-
-
-router.register(r'api/v1/users', UserViewSet, basename='users')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls')),
+    path('', include('authentication.urls', namespace='authentication')),
     path('', include('recipes.urls', namespace='recipes')),
-    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
     path('silk/', include('silk.urls', namespace='silk'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
