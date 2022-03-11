a5808066711b02ecd95fa17b390b44fbc57c7c21
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-10 21:29:44 -0300

    :test_tube: test: detail and update tag

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,4 @@
+from random import randint
 
 from model_bakery import baker
 from django.urls import reverse
@@ -27,3 +28,37 @@ class TagListCreateViewTest(APITestCase):
         
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['slug'], Tag.objects.last().slug)
+    
+    def test_get_tag(self):
+        user = baker.make(User, is_superuser=True)
+        tag = baker.make(Tag, user=user, name='New Tag')
+        self.client.force_authenticate(user)
+        url = reverse('recipes:tags-detail', args=[tag.id])
+        response = self.client.get(url, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['slug'], tag.slug)
+        self.assertEqual(response.data['name'], tag.name)
+    
+    def test_put_tag_not_found(self):
+        user = baker.make(User, is_superuser=True)
+        
+        self.client.force_authenticate(user)
+        url = reverse('recipes:tags-detail', args=[randint(500, 1000)])
+        response = self.client.put(url, {}, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_put_tag(self):
+        user = baker.make(User, is_superuser=True)
+        tag = baker.make(Tag, name='New Tag', user=user)
+        payload = dict(name='New Tag 2')
+        self.client.force_authenticate(user)
+        url = reverse('recipes:tags-detail', args=[tag.id])
+        response = self.client.put(url, payload, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], Tag.objects.last().id)
+        self.assertEqual(response.data['name'], Tag.objects.last().name)
+        self.assertEqual(response.data['slug'], Tag.objects.last().slug)
+        
