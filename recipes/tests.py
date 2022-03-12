0dded4ba0ac7cbe411c4de77954dd050d9f00951
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-12 20:35:12 -0300

    :test_tube: test: destroy tag

--- recipes/tests.py
+++ recipes/tests.py
@@ -61,4 +61,20 @@ class TagListCreateViewTest(APITestCase):
         self.assertEqual(response.data['id'], Tag.objects.last().id)
         self.assertEqual(response.data['name'], Tag.objects.last().name)
         self.assertEqual(response.data['slug'], Tag.objects.last().slug)
-        
+    
+    def test_destroy_tag(self):
+        user = baker.make(User, is_superuser=True)
+        tag = baker.make(Tag, name='New Tag Destroy', user=user, is_active=True)
+        self.client.force_authenticate(user)
+        url = reverse('recipes:tags-detail', args=[tag.id])
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_destroy_tag_not_found(self):
+        user = baker.make(User, is_superuser=True)
+        baker.make(Tag, name='New Tag Destroy', user=user)
+        self.client.force_authenticate(user)
+        url = reverse('recipes:tags-detail', args=[randint(500, 1000)])
+        response = self.client.delete(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
