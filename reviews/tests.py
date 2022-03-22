d5d67ea3a9a90c1035224f3d6e02e99da4befd93
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-21 23:33:01 -0300

    :test_tube: test: list reviews recipes

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from model_bakery import baker
 
-# Create your tests here.
+from .models import Recipe, ReviewRecipe
+from authentication.models import User
+
+
+class ReviewRecipeListTest(APITestCase):
+    def test_reviewrecipe_list(self):
+        user = baker.make(User, is_superuser=True)
+        [baker.make(ReviewRecipe, is_active=True) for _ in range(0,10)]
+        self.client.force_authenticate(user)
+        response = self.client.get(
+            reverse('reviews:review_recipes-list'), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 10)
