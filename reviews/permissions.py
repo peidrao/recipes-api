50c2d28aed922193ecfef0941bf9ce7dcc9b1a72
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-21 23:33:22 -0300

    :sparkles: feat:add get in permission

--- reviews/permissions.py
+++ reviews/permissions.py
@@ -5,6 +5,9 @@ from .models import ReviewRecipe
 class HasOnlyReview(permissions.BasePermission):
     message = 'You can only rate a recipe once'
     def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
         if ReviewRecipe.objects.filter(user=request.user, recipe_id=request.data['recipe']).count() >= 1:
             return False
         return True
