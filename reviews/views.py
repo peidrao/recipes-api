498b1195938f633301e99dc460299064a207eaa0
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-22 22:35:52 -0300

    :sparkles: feat: remove commets

--- reviews/views.py
+++ reviews/views.py
@@ -7,12 +7,6 @@ from .serializers import ReviewRecipeSerializer
 from .models import ReviewRecipe
 
 
-
-
-# Validações
-# O usuário só pode fazer uma avaliação para cada receita
-
-
 class ReviewRecipeList(generics.ListCreateAPIView):
     queryset = ReviewRecipe.objects.filter(is_active=True)
     serializer_class = ReviewRecipeSerializer
