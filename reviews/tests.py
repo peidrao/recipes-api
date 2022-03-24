e0c963667e76928ba2bcefe00512fc9ca3d5a420
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-24 19:43:37 -0300

    :test_tube: test: delete and get reviews

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,4 @@
+from random import randint
 from rest_framework import status
 from django.urls import reverse
 from rest_framework.test import APITestCase
@@ -40,4 +41,39 @@ class ReviewRecipeListTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(response.data['detail'].title(), 'You Can Only Rate A Recipe Once')
         
+
+class ReviewRecipeDetailTest(APITestCase):
+    def test_reviewrecipe_get_by_id(self):
+        user = baker.make(User, is_superuser=True)
+        review = baker.make(ReviewRecipe, is_active=True)
+        self.client.force_authenticate(user)
+        response = self.client.get(
+            reverse('reviews:review_recipes-detail', args=[review.id]), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_reviewrecipe_get_by_id_not_found(self):
+        user = baker.make(User, is_superuser=True)
+        self.client.force_authenticate(user)
+        response = self.client.get(
+            reverse('reviews:review_recipes-detail', args=[randint(500, 1000)]), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_reviewrecipe_delete(self):
+        user = baker.make(User, is_superuser=True)
+        review = baker.make(ReviewRecipe, is_active=True)
+        self.client.force_authenticate(user)
+        response = self.client.delete(
+            reverse('reviews:review_recipes-detail', args=[review.id]), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         
+    def test_reviewrecipe_delete_not_found(self):
+        user = baker.make(User, is_superuser=True)
+        
+        self.client.force_authenticate(user)
+        response = self.client.delete(
+            reverse('reviews:review_recipes-detail', args=[randint(500, 1000)]), format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
