cd95d2f854682906102e92c900ab18066ec6a068
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-24 19:06:07 -0300

    :test_tube: test: create reviews recipes

--- reviews/tests.py
+++ reviews/tests.py
@@ -17,3 +17,27 @@ class ReviewRecipeListTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 10)
+
+    def test_reviewrecipe_create(self):
+        user = baker.make(User, is_superuser=True)
+        recipe = baker.make(Recipe)
+        payload = dict(user=user.id, recipe=recipe.id, rate=5, comment='Best')
+        self.client.force_authenticate(user)
+        response = self.client.post(reverse('reviews:review_recipes-list'), payload, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['rate'], ReviewRecipe.objects.last().rate)
+        self.assertEqual(response.data['comment'], ReviewRecipe.objects.last().comment)
+    
+    def test_reviewrecipe_dont_rate_same_recipe(self):
+        user = baker.make(User, is_superuser=True)
+        recipe = baker.make(Recipe)
+        baker.make(ReviewRecipe, recipe_id=recipe.id, user_id=user.id)
+        payload = dict(user=user.id, recipe=recipe.id, rate=5, comment='Best')
+        self.client.force_authenticate(user)
+        response = self.client.post(reverse('reviews:review_recipes-list'), payload, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'].title(), 'You Can Only Rate A Recipe Once')
+        
+        
