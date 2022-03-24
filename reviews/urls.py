c449100f05e80645edaa49a7c2ae311ebb596ee9
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-24 19:31:31 -0300

    :sparkles: feat: reviews details service

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ReviewRecipeList
+from .views import ReviewRecipeDetail, ReviewRecipeList
 
 app_name = 'reviews'
 
 urlpatterns = [
-    path('api/v1/review/recipes/', ReviewRecipeList.as_view(), name='review_recipes-list')
+    path('api/v1/review/', ReviewRecipeList.as_view(), name='review_recipes-list'),
+    path('api/v1/review/<int:pk>', ReviewRecipeDetail.as_view(), name='review_recipes-detail')
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -11,3 +11,9 @@ class ReviewRecipeList(generics.ListCreateAPIView):
     queryset = ReviewRecipe.objects.filter(is_active=True)
     serializer_class = ReviewRecipeSerializer
     permission_classes = (IsAuthenticated, HasOnlyReview)
+
+
+class ReviewRecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ReviewRecipe.objects.filter(is_active=True)
+    serializer_class = ReviewRecipeSerializer
+    permission_classes = (IsAuthenticated,)
