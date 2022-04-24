4839b7b66c7d87f8119ed045c890a520ba97926e
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-24 11:39:00 -0300

    :truck: refactor - unused code

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,39 +5,22 @@ from .models import Recipe, Tag, Ingredient
 
 from authentication.serializers import UserSerializer
 
+
 class TagSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(read_only=True)
     is_active = serializers.BooleanField(read_only=True)
-    user = serializers.SerializerMethodField()
 
     class Meta:
         model = Tag
         fields = "__all__"
-    
-    def create(self, validated_data):
-        instance = super(TagSerializer, self).create(validated_data)
-        instance.user = self.context['request'].user
-        instance.save()
-        return instance
-    
-    def get_user(self, obj):
-        if obj.user:
-            return dict(id=obj.id, username=obj.user.username, email=obj.user.email)
-        return {}
 
 
 class IngredientSerializer(serializers.ModelSerializer):
 
-    class Meta: 
+    class Meta:
         model = Ingredient
         fields = '__all__'
 
-    def create(self, validated_data):
-        instance = super(IngredientSerializer, self).create(validated_data)
-        instance.user = self.context['request'].user
-        instance.save()
-        return instance
-    
 
 class RecipeSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
@@ -47,7 +30,8 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('title', 'price', 'time_minutes', 'ingredients', 'tags', 'image', 'user', 'reviews')
+        fields = ('title', 'price', 'time_minutes', 'ingredients',
+                  'tags', 'image', 'user', 'reviews')
 
     def get_user(self, obj):
         if obj.user:
@@ -71,7 +55,7 @@ class RecipeSerializer(serializers.ModelSerializer):
             i['name'] = tag.name
             i['slug'] = tag.slug
             json.append(i)
-        
+
         return json
 
     def get_ingredients(self, obj):
@@ -79,7 +63,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_reviews(self, obj):
         reviews = ReviewRecipe.objects.filter(recipe_id=obj.id)
-        
+
         json = []
         if reviews.exists():
             for i in reviews:
